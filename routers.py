b7405d692f41c4b19be3d61db61f194900f84aa2
André Perez <andre.perez@dengun.com>
2022-11-08 00:57:37 -0300

    ADD: Book API router

---
+++ routers.py
@@ -0,0 +1,125 @@
+"""
+Book API router
+"""
+
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Query, status
+from fastapi_cache.decorator import cache
+from sqlalchemy.orm import Session
+
+import repository
+import schemas
+from database import get_db
+
+book_router = APIRouter(tags=["books"])
+
+
+@book_router.get(
+    "/book",
+    response_model=schemas.APIResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Search for books by title",
+)
+@cache(expire=60)
+async def search_books(search: str, page: Optional[int] = Query(default=None, ge=1)):
+    """
+    Search for books by title
+
+    :param search: Search term
+    :param page: Page number
+
+    :return: List of books
+    """
+    
+    results = await repository.get_books_by_title(search, page)
+    return results
+
+
+@book_router.get(
+    "/book/top-rated",
+    response_model=schemas.BookListWithReview,
+    status_code=status.HTTP_200_OK,
+    summary="Get top rated books",
+)
+async def get_top_rated_books(
+    limit: Optional[int] = Query(default=10, ge=1),
+    db: Session = Depends(get_db)
+):
+    """
+    Get top rated books
+
+    :param limit: Number of results to return
+    :param db: Database session
+
+    :return: List of top rated books
+    """
+
+    results = await repository.get_top_rated_books(limit, db)
+    return {"books": results}
+
+
+@book_router.get(
+    "/book/{book_id}/monthly-rating",
+    response_model=schemas.MonthlyRatingList,
+    status_code=status.HTTP_200_OK,
+    summary="Get book monthly rating",
+)
+async def get_monthly_rating(book_id: int, db: Session = Depends(get_db)):
+    """
+    Get book monthly rating
+
+    :param book_id: Book ID
+    :param db: Database session
+
+    :return: Book monthly rating
+    """
+
+    results = await repository.get_monthly_rating(book_id, db)
+    return results
+
+
+@book_router.post(
+    "/book/{book_id}/review",
+    response_model=schemas.BookReviewWithRatingAndBookID,
+    status_code=status.HTTP_201_CREATED,
+    summary="Add a review for a book",
+)
+async def add_review(
+    book_id: int,
+    book_review: schemas.BookReviewAndRating,
+    db: Session = Depends(get_db),
+):
+    """
+    Add a review for a book
+
+    :param book_id: Book ID
+    :param book_review: Book review
+    :param db: Database session
+
+    :return: Book with review
+    """
+
+    review = await repository.create_review(book_id, book_review, db)
+    return review
+
+
+@book_router.get(
+    "/book/{book_id}",
+    response_model=schemas.BookWithReview,
+    status_code=status.HTTP_200_OK,
+    summary="Get book details",
+)
+@cache(expire=60)
+async def get_book(book_id: int, db: Session = Depends(get_db)):
+    """
+    Get book details, with reviews and rating
+
+    :param book_id: Book ID
+    :param db: Database session
+
+    :return: Book details
+    """
+
+    results = await repository.get_book_with_review(book_id, db)
+    return results
