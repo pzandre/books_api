618b4fad6b436d6ebd38e32342ceae767940cf2f
André Perez <andre.perez@dengun.com>
2022-11-08 00:55:13 -0300

    ADD: Database settings

---
+++ database.py
@@ -0,0 +1,29 @@
+"""
+Project database settings
+"""
+
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./books.db")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    """
+    This function is used to get database session
+    """
+
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
