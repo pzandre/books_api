2ce0e55e794d7cc722dbacffd56710e9905d9258
André Perez <andre.perez@dengun.com>
2022-11-08 00:57:14 -0300

    ADD: Project constants

---
+++ constants.py
@@ -0,0 +1,23 @@
+"""
+Project constants
+"""
+
+import os
+
+EXTERNAL_API_URL = os.getenv("EXTERNAL_API_URL", "https://gutendex.com/books/")
+PROJECT_URL = os.getenv("PROJECT_URL", "http://localhost:8000")
+
+MONTHS = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
